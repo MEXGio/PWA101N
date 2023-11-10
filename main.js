b3466c29e0c7a4d7409fde7016815c21d1eca0da
Giovanni Lara <glb06292002@gmail.com>
2023-11-10 17:57:16 -0600

    primer commit

---
+++ main.js
@@ -0,0 +1,8 @@
+if('serviceWorker' in navigator){
+    console.log("Andando service worker");
+    navigator.serviceWorker.register("./sw.js")
+    .then(res=>console.log('serviceWorker cargado correctamente', res))
+    .catch(err=>console.log('serviceWorker no se ha podido registrar',err));
+}else{
+    console.log("Aun no lo puedes usar");
+}
